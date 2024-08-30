655b2e4fbc93ba1da1935c1bc71ed166732c832a
Mahammed Sarfaraz Nawaz Pagala <pmdnawaz1@gmail.com>
2024-08-31 00:02:28 +0530

    add gtag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -49,14 +49,21 @@ export default class MyDocument extends Document {
             `,
           }}
         />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-HZJY98K1XE"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-HZJY98K1XE"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
 
-          gtag('config', 'G-HZJY98K1XE');
-        </script>
+              gtag('config', 'G-HZJY98K1XE');
+            `,
+          }}
+        />
         </Head>
         <body>
           <Main />
